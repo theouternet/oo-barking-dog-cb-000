f53930cbff74013bacdf2e5ada0b788dfb6ee12b
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-07-31 23:20:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
 # Your code goes here!
 
 class Dog 
-  def initialize 
+  def initialize(name)
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+end
